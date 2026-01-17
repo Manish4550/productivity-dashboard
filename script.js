7b90dd15e161d8ab5e6c90f3a87a9f3a925ba772
Manish  Kumar <Amity4933@gmail.com>
2026-01-18 03:17:49 +0530

    add more function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+function openFeatures() {
 let allelem = document.querySelectorAll('.elem')
 let allelempage =document.querySelectorAll('.fullelem') 
 let allfullelembackbtn=document.querySelectorAll('.fullelem .back')
@@ -14,3 +16,59 @@ allfullelembackbtn.forEach(function(back){
         allelempage[back.id].style.display='none'
     })
 })
+}
+
+openFeatures()
+
+let form =document.querySelector('.addTask form')
+let taskInput =document.querySelector('.addTask form #task-input')
+let taskDetailsInput =document.querySelector('.addTask form textarea')
+let taskChecked =document.querySelector('.addTask form #check')
+
+let currentTask = [
+    {
+        task:"Mandir jao",
+        details:"Durga Maa",
+        imp:true
+    },
+    {
+        task:"Recording kro",
+        details:"Cohot ki",
+        imp:true
+    },
+    {
+        task:"Study kro",
+        details:"College me",
+        imp:false
+    }
+]
+
+function renderTask(){
+
+    let allTask=document.querySelector('.allTask')
+   let sum = ''
+currentTask.forEach(function(elem){
+    
+    sum = sum +  `  <div class="task">
+              <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+            <button>Marked As Completed</button>
+          </div>`
+    
+})
+
+allTask.innerHTML=sum
+
+}
+
+renderTask()
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    // console.log(taskInput.value);
+    // console.log(taskDetailsInput.value);
+    // console.log(taskChecked.checked);
+    console.log(currentTask);
+    
+})
+
+
