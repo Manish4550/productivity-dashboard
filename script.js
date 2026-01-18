f489398debfdba8297b6906c0cc9d040c3ff7fe9
Manish  Kumar <Amity4933@gmail.com>
2026-01-18 16:54:25 +0530

    Daily Planner Completed

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ function renderTask() {
 
  
   
-  let allTask = document.querySelector(".allTask");
-  let sum = "";
+  var allTask = document.querySelector(".allTask");
+  var sum = "";
   currentTask.forEach(function (elem,idx) {
     sum =
       sum +
@@ -85,3 +85,39 @@ form.addEventListener("submit", function (e) {
 }
 
 todolist()
+
+
+function dailyPlanner() {
+  
+var dayPlanner = document.querySelector('.day-planner')
+var dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {}
+
+var hours =Array.from({length:18},(_,idx)=>`${6+idx}:00 - ${7+idx}:00`)
+
+var wholeDaySum = ''
+hours.forEach(function(elem,idx){
+
+  var savedData = dayPlanData[idx] || ''
+  wholeDaySum = wholeDaySum +  ` <div class="day-planner-time">
+            <p>${elem}</p>
+            <input id=${idx} type="text" placeholder="..." value=${savedData}>
+          </div>`
+
+})
+
+
+
+dayPlanner.innerHTML = wholeDaySum
+
+var dayPlannerInput = document.querySelectorAll('.day-planner input')
+
+dayPlannerInput.forEach(function(elem){
+  elem.addEventListener('input', function(){
+     dayPlanData[elem.id] = elem.value
+    
+     localStorage.setItem('dayPlanData',JSON.stringify(dayPlanData))
+  })
+})
+}
+
+dailyPlanner()
