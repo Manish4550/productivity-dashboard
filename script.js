bbea20a9483f9e662c8f38f2a4decec79e97f70c
Manish  Kumar <Amity4933@gmail.com>
2026-01-18 14:58:25 +0530

    Working on Daily Planner

--- script.js
+++ script.js
@@ -1,74 +1,87 @@
-
 function openFeatures() {
-let allelem = document.querySelectorAll('.elem')
-let allelempage =document.querySelectorAll('.fullelem') 
-let allfullelembackbtn=document.querySelectorAll('.fullelem .back')
+  let allelem = document.querySelectorAll(".elem");
+  let allelempage = document.querySelectorAll(".fullelem");
+  let allfullelembackbtn = document.querySelectorAll(".fullelem .back");
 
-allelem.forEach(function(elem){
-    elem.addEventListener('click', function(){
-        allelempage[elem.id].style.display = 'block'
-    })
-    
-})
+  allelem.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      allelempage[elem.id].style.display = "block";
+    });
+  });
 
-allfullelembackbtn.forEach(function(back){
-    back.addEventListener('click', function(){ 
-        allelempage[back.id].style.display='none'
-    })
-})
+  allfullelembackbtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      allelempage[back.id].style.display = "none";
+    });
+  });
 }
 
-openFeatures()
+openFeatures();
 
-let form =document.querySelector('.addTask form')
-let taskInput =document.querySelector('.addTask form #task-input')
-let taskDetailsInput =document.querySelector('.addTask form textarea')
-let taskChecked =document.querySelector('.addTask form #check')
-
-let currentTask = [
-    {
-        task:"Mandir jao",
-        details:"Durga Maa",
-        imp:true
-    },
-    {
-        task:"Recording kro",
-        details:"Cohot ki",
-        imp:true
-    },
-    {
-        task:"Study kro",
-        details:"College me",
-        imp:false
-    }
-]
-
-function renderTask(){
-
-    let allTask=document.querySelector('.allTask')
-   let sum = ''
-currentTask.forEach(function(elem){
+function todolist(){
+  var currentTask = [];
+if(localStorage.getItem('currentTask')){
+    currentTask =JSON.parse(localStorage.getItem('currentTask'))
+}else{
+    console.log('Task List is Empty');
     
-    sum = sum +  `  <div class="task">
-              <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
-            <button>Marked As Completed</button>
-          </div>`
-    
-})
+}
 
-allTask.innerHTML=sum
+function renderTask() {
+
+ 
+  
+  let allTask = document.querySelector(".allTask");
+  let sum = "";
+  currentTask.forEach(function (elem,idx) {
+    sum =
+      sum +
+      `  <div class="task">
+        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+        <button id=${idx}>Marked As Completed</button>
+        </div>`;
+  });
+
+  allTask.innerHTML = sum;
+
+   localStorage.setItem('currentTask',JSON.stringify(currentTask))
+   
+   // Attach event listeners to newly created buttons
+   let markedCompletedBtn = document.querySelectorAll('.task button')
+   markedCompletedBtn.forEach(function(btn){
+     btn.addEventListener('click', function(){
+       currentTask.splice(btn.id, 1)
+       renderTask()
+     })
+   })
+}
+
+renderTask();
+
+let form = document.querySelector(".addTask form");
+let taskInput = document.querySelector(".addTask form #task-input");
+let taskDetailsInput = document.querySelector(".addTask form textarea");
+let taskChecked = document.querySelector(".addTask form #check");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // console.log(taskInput.value);
+  // console.log(taskDetailsInput.value);
+  // console.log(taskChecked.checked);
+  currentTask.push({
+    task: taskInput.value,
+    details: taskDetailsInput.value,
+    imp: taskChecked.checked,
+  });
+  renderTask();
+  
+  taskInput.value=''
+  taskDetailsInput.value=''
+  taskChecked.checked=false
+
+  
+});
 
 }
 
-renderTask()
-
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    // console.log(taskInput.value);
-    // console.log(taskDetailsInput.value);
-    // console.log(taskChecked.checked);
-    console.log(currentTask);
-    
-})
-
-
+todolist()
